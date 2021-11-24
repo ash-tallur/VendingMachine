cfa007cec615ad0679297c3a2b164fea2b9c62e6
Ashwini <ashanika25@gmail.com>
2021-11-25 00:18:35 +0530

    Changes in use of global variables

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -22,98 +22,84 @@ import java.util.Scanner;
 
 public class VendingMachine {
 
-    int option;
-    float userAmount;
-    float userBalanceAmount;
     float amountInMachine = 0;
-    float productPrice;
-    String productName;
     Scanner sc;
 
-        public static void main(String[] args) {
-            VendingMachine machine = new VendingMachine();
-            while(true){
-                machine.initialItemsDisplay();
-                machine.checkOnUserInput();
-                machine.choiceOfItem();
-            }
+    public VendingMachine() {
+        sc = new Scanner(System.in);
+    }
+
+    public static void main(String[] args) {
+        VendingMachine machine = new VendingMachine();
+        while (true) {
+            int option = machine.initialItemsDisplay();
+            machine.checkOnUserInput(option);
+            machine.choiceOfItem(option);
         }
+    }
 
-        public void initialItemsDisplay(){
-            System.out.println("HELLO!!! Look into the below options for the items and opt");
-            System.out.println(" Item Name           Price \n 1. Cool Drink      Rs. 50.00 \n 2. Energy Bar      Rs. 35.50 \n 3. Masks           Rs. 80.25 \n 4. Coffee          Rs. 20.00 \n 5. Chips           Rs. 40.00 \n 6. Check Amount in Machine");
-            System.out.println("Choose the item");
-            sc = new Scanner(System.in);
-            option = sc.nextInt();
+    public int initialItemsDisplay() {
+        System.out.println("HELLO!!! Look into the below options for the items and opt");
+        System.out.println(" Item Name           Price \n 1. Cool Drink      Rs. 50.00 \n" +
+                " 2. Energy Bar      Rs. 35.50 \n 3. Masks           Rs. 80.25 \n 4. Coffee          Rs. 20.00 \n" +
+                " 5. Chips           Rs. 40.00 \n 6. Check Amount in Machine");
+        System.out.println("Choose the item");
+        return sc.nextInt();
+    }
+
+    public void checkOnUserInput(int option) {
+        if (option < 1 || option > 6) {
+            System.out.println("Choose the right option");
+        } else if (option == 6) {
+            System.out.println("The amount in the machine " + amountInMachine);
         }
+    }
 
-        public void checkOnUserInput(){
-
-            if(option <1 || option >6){
-                System.out.println("Choose the right option");
-            }
-            else if(option == 6){
-                System.out.println("The amount in the machine " + amountInMachine);
-            }
-
-        }
-
-        public void choiceOfItem(){
-            if(option ==1){
+    public void choiceOfItem(int option) {
+        float productPrice = 0;
+        String productName = "";
+        switch (option) {
+            case 1:
                 productPrice = 50.00F;
                 productName = "Cold Drink";
-                System.out.println("Load amount " + productPrice + " for " + productName);
-                userAmount = sc.nextFloat();
-                itemPurchase(productPrice,userAmount,productName);
-            }
-            else if(option == 2){
+                break;
+            case 2:
                 productPrice = 35.50F;
                 productName = "Energy Bar";
-                System.out.println("Load amount " + productPrice + " for " + productName);
-                userAmount = sc.nextFloat();
-                itemPurchase(productPrice,userAmount,productName);
-            }
-            else if(option == 3){
+                break;
+            case 3:
                 productPrice = 80.25F;
                 productName = "Masks";
-                System.out.println("Load amount " + productPrice + " for " + productName);
-                userAmount = sc.nextFloat();
-                itemPurchase(productPrice,userAmount,productName);
-            }
-            else if(option == 4){
+                break;
+            case 4:
                 productPrice = 20.00F;
                 productName = "Coffee";
-                System.out.println("Load amount " + productPrice + " for " + productName);
-                userAmount = sc.nextFloat();
-                itemPurchase(productPrice,userAmount,productName);
-            }
-            else if(option == 5){
+                break;
+            case 5:
                 productPrice = 40.00F;
                 productName = "Chips";
-                System.out.println("Load amount " + productPrice + " for " + productName);
-                userAmount = sc.nextFloat();
-                itemPurchase(productPrice,userAmount,productName);
-            }
+                break;
         }
+        System.out.println("Load amount " + productPrice + " for " + productName);
+        float userAmount = sc.nextFloat();
+        itemPurchase(productPrice, userAmount, productName);
+    }
 
-        public void itemPurchase(float productPrice,float userAmount, String productName){
+    public void itemPurchase(float productPrice, float userAmount, String productName) {
+        if (userAmount > 2000) {
+            System.out.println("Highest denomination is 2000, please load less than Rs.2000");
+        } else if (userAmount < productPrice) {
+            System.out.println("Please load the exact amount of product");
+        } else {
+            System.out.println("Amount loaded by you: " + userAmount);
+            float userBalanceAmount = userAmount - productPrice;
 
-            if(userAmount>2000){
-                System.out.println("Highest denomination is 2000, please load less than Rs.2000");
+            if (userBalanceAmount == 0) {
+                System.out.println("Dispensing" + productName);
+            } else if (userBalanceAmount > 0) {
+                System.out.println("Please collect " + productName + " and your balance of Rs." + userBalanceAmount);
             }
-            else if(userAmount<productPrice){
-                System.out.println("Please load the exact amount of product");
-            }
-            else{
-                System.out.println("Amount loaded by you: " + userAmount);
-                userBalanceAmount = userAmount - productPrice;
-
-                if (userBalanceAmount == 0) {
-                    System.out.println("Dispensing" + productName);
-                } else if (userBalanceAmount > 0) {
-                    System.out.println("Please collect " + productName +" and your balance of Rs." + userBalanceAmount);
-                }
-                amountInMachine += productPrice;
-            }
+            amountInMachine += productPrice;
         }
+    }
 }
